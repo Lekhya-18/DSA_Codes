cb094fa76a74b1b5751368c1837456276209a74e
Lekhya <lekhyapitta@gmail.com>
2025-10-01 09:31:53 +0530

    My first singlell commit

---
+++ Singlell.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int choice,value,key,position;
+    printf("Single linked list");
+    printf("Operations that can be performed:\n");
+    printf("_________MENU_________\n");
+    printf("1.Insert At Beginning\n 2.Insert At End 3.Insert Next to specific key \n 
+        4.Insert at the position from beginning\n 5.Insert at the position from end \n 
+        6.Delete First Node\n 7.Delete Last NOde\n 8.Delete next to key\n 9.Delete wrt Position\n
+        10.Search key\n 11.Traverse\n 12.Exit\n");
+    while(1){
+    printf("Select Your Choice:");
+    scanf("%d",&choice);
+    switch(choice){
+        
+    }
+
+    }    
+    return 0;
+}
